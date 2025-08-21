040946740c2e06b25f89c9a5023a3fcf921596dd
Jurre Dijkstra <jurredijkstra@mijnheem.nl>
2025-08-21 21:08:38 +0200

    first commit

---
+++ dirtyZero/dirtyZeroApp.swift
@@ -0,0 +1,19 @@
+//
+//  dirtyZeroApp.swift
+//  dirtyZero
+//
+//  Created by Skadz on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct dirtyZeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+extension String: @retroactive Error {}
